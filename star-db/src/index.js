c268f36f5a4b7fa86c1e151bc26f84d8b0c9f07d
alexey <mazurenko.alexey@mail.ru>
2019-06-18 06:43:04 +0300

    доработка запроса

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,12 +1,21 @@
 const getResource = async url => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` + `, received ${res.status}`);
+  }
+
   const body = await res.json();
   return body;
 };
 
-getResource('https://swapi.co/api/people/1/').then(body => {
-  console.log(body);
-});
+getResource('https://swapi.co/api/people/16546546/')
+  .then(body => {
+    console.log(body);
+  })
+  .catch(err => {
+    console.error('Console not fetch', err);
+  });
 
 /*fetch('https://swapi.co/api/people/1/')
   .then(res => {
